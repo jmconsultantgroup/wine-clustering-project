29922b4c4849af95aa5c798893ff00ff5c5f2622
Jonathan Ware <jonathan.ware02@gmail.com>
2023-07-20 16:42:21 -0500

    ran more visualization and clustering

--- explore.py
+++ explore.py
@@ -46,32 +46,30 @@ def plot_variable_pairs(df):
     
     
 
-def plot_categorical_and_continuous_vars(df):
-    '''This function graphs a swarmplot, lmplot, stripplot, and violinplot to visualize the relationship between categorical and continuous variables.'''
-   
+def plot_continuous_vs_categorical(df, cat_var_col, con_var_col):
 
-   
-    # sample the data to make the graph readable
-    
-    plt.figure(figsize=(18, 8)) 
-
-    # Swarm Plot
-    plt.subplot(1, 4, 1)
-    sns.swarmplot(x=cat_var_col, y=con_var_col, data=df)
-    plt.title('Swarm Plot')
-
- 
-    # Strip Plot
-    plt.subplot(1, 4, 2)
-    sns.stripplot(x=cat_var_col, y=con_var_col, data=df)
-    plt.title('Strip Plot')
-
-     # Histogram
-    plt.subplot(1, 4, 3)
-    sns.histplot(x=con_var_col, data=train)
-    plt.title('Histogram')
-
-
-    plt.tight_layout()
-    plt.show()
-  
+    for con_var in con_var_col:
+        plt.figure(figsize=(12, 4))
+        
+        
+        # Long-form DataFrame
+        data_long = df.melt(id_vars=[cat_var_col], value_vars=[con_var], var_name='Variable', value_name='Value')
+        
+        # Swarm Plot
+        plt.subplot(1, 3, 1)
+        sns.swarmplot(x=cat_var_col, y='Value', data=data_long)
+        plt.title(f'{con_var} vs {cat_var_col}')
+        
+        # Histogram
+        plt.subplot(1, 3, 2)
+        sns.histplot(data=data_long, x='Value', hue=cat_var_col, kde=True)
+        plt.title(f'{con_var} Histogram')
+        
+        # Bar Plot
+        plt.subplot(1, 3, 3)
+        mean_values = df.groupby(cat_var_col)[con_var].mean().reset_index()
+        sns.barplot(x=cat_var_col, y=con_var, data=mean_values, ci=None)
+        plt.title(f'Mean {con_var} Bar Plot')
+        
+        plt.tight_layout()
+        plt.show()
